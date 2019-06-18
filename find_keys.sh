c6f5f07c11e41aef62df4b4a23e1c206e0221413
Jonathan A Rees <you@example.com>
2019-06-18 14:59:53 -0500

    fix find_keys.sh

--- find_keys.sh
+++ find_keys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~/bhl
+
 TAB=`echo -e "\t"`
 
 # Get table of file name, line number, text matching regex.  Colon-separated.
@@ -13,9 +15,14 @@ find data -type f -exec grep -n name /dev/null {} \; \
 # Get table of hash, barcode.  Tab-separated.  Note presence of
 # tab character in this shell command.
 
-preston log -l tsv | grep hasVersion | grep -v well-known | \
-   sed -e "s+.*/download/\(.*\)/mobot.*sha256/\(.*\)> .+\2$TAB\1+" \
-   > hash-barcode.tsv
+preston log -l tsv | grep archive.*hasVersion | grep -v well-known \
+   | sed -e "s+https://archive.org/download/\([^/]*\).*sha256/\(.*\)+\2$TAB\1+" \
+   | head -3
+
+# Last ran: cat foo.tsv |  sed -e 's+.*/download/\(.*\)/mobot.*sha256/\(.*\)> .+\1	\2+' | head -1
+# Last ran: cat foo.tsv |  sed -e 's+.*/download/\(.*\)/mobot.*sha256/\(.*\)> .+\2	\1+' | head -1
+# Last ran: cat foo.tsv |  sed -e 's+.*/download/\(.*\)/mobot.*sha256/\(.*\)> .+\2$TAB\1+' | head -1
+# Last ran: cat foo.tsv |  sed -e "s+.*/download/\(.*\)/mobot.*sha256/\(.*\)> .+\2$TAB\1+" | head -1
 
 # Get table of barcode, ... itemurl
 
@@ -25,3 +32,4 @@ preston log -l tsv | grep item.txt | grep hasVersion | head -n 1 | cut -f 3 | pr
 # Join 
 
 join <(sort file-line-key.txt) <(sort 
+
